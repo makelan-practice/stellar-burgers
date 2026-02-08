dcc64f4e1251d492fedf6da9d7f72169d7fd9920
makeev <makeevi2004@mail.ru>
2026-02-08 12:08:41 +0300

    Переработан редактор ингредиентов в burgerConstructorSlice.

--- src/services/slices/burgerConstructorSlice.ts
+++ src/services/slices/burgerConstructorSlice.ts
@@ -17,15 +17,27 @@ const burgerConstructorSlice = createSlice({
   name: 'burgerConstructor',
   initialState,
   reducers: {
-    addIngredient: (state, action: PayloadAction<TIngredient>) => {
-      const ingredient = action.payload;
+    addIngredient: {
+      reducer: (
+        state,
+        action: PayloadAction<TIngredient | TConstructorIngredient>
+      ) => {
+        const ingredient = action.payload;
 
-      if (ingredient.type === 'bun') {
-        state.bun = ingredient;
-        return;
+        if (ingredient.type === 'bun') {
+          state.bun = ingredient as TIngredient;
+          return;
+        }
+
+        state.ingredients.push(ingredient as TConstructorIngredient);
+      },
+      prepare: (ingredient: TIngredient) => {
+        if (ingredient.type === 'bun') {
+          return { payload: ingredient };
+        }
+
+        return { payload: { ...ingredient, id: uuidv4() } };
       }
-
-      state.ingredients.push({ ...ingredient, id: uuidv4() });
     },
     removeIngredient: (state, action: PayloadAction<string>) => {
       state.ingredients = state.ingredients.filter(
